f269e3c869a1e0926b24d85fe73ca349fca54e5f
lyh71709 <60583161+lyh71709@users.noreply.github.com>
2022-02-28 14:49:59 +1300

    Stalling

--- 00_sandbox.py
+++ 00_sandbox.py
@@ -1,6 +1,20 @@
-from datetime import datetime
+#Import required libraries
+from tkinter import *
+#Create an instance of tkinter frame
+win= Tk()
+#Define the geometry of the window
+win.geometry("750x250")
+#Define functions
+def on_enter(e):
+   button.config(background='OrangeRed3', foreground= "white")
 
-now = datetime.now()
+def on_leave(e):
+   button.config(background= 'SystemButtonFace', foreground= 'black')
+#Create a Button
+button= Button(win, text= "Click Me", font= ('Helvetica 13 bold'))
+button.pack(pady= 20)
 
-current_time = now.strftime("%y")
-print("Current Time =", current_time)
+#Bind the Enter and Leave Events to the Button
+button.bind('<Enter>', on_enter)
+button.bind('<Leave>', on_leave)
+win.mainloop()
